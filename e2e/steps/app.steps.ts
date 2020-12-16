ca21da35cb831dc890a3fe4818959455c3c621c0
Tim Nederhoff <post@timnederhoff.nl>
2020-12-16 13:24:18 +0100

    default timeout for app steps

--- e2e/steps/app.steps.ts
+++ e2e/steps/app.steps.ts
@@ -1,22 +1,23 @@
-const { Given, When, Then } = require('cucumber');
+const { Given, When, Then, setDefaultTimeout } = require('cucumber');
 const { browser } = require('protractor');
 const chai = require('chai');
 chai.use(require('chai-as-promised'));
+setDefaultTimeout(60 * 1000);
 
 const searchFormPO = require('../page-objects/search-form.po');
 
-Given('I navigate to {string}', { timeout: 60 * 1000 }, async (string) => {
+Given('I navigate to {string}', async (string) => {
     await browser.get('http://' + string + ':4200/');
     await browser.sleep(2000);
 });
 
-When('I search for Luke Skywalkers name', { timeout: 60 * 1000 }, async () => {
+When('I search for {string}s name', async () => {
     await searchFormPO.input.sendKeys('Luke Skywalker');
     await searchFormPO.searchBtn.click();
     await browser.sleep(2000);
 });
 
-Then('I see Lukes details', { timeout: 60 * 1000 }, async () => {
+Then('I see Lukes details', async () => {
     await chai.expect(searchFormPO.firstCharacterName.getAttribute('innerText'))
         .to.eventually.contain('Skywalker');
 });
